6ff1919204f1b92012b89d8ae2368492d2e70f82
Bhim <vanwu012@gmail.com>
2024-08-16 16:20:11 +0545

    changed

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,6 @@
 // // const title = document.getElementById("heading")
 // // console.log(title)
- 
+
 // // const listItem = document.getElementsByClassName("list-item")
 // console.log(listItem)
 
@@ -10,14 +10,24 @@
 // const container = document.querySelector("div")
 // console.log(conatiner)
 
-const title = document.querySelector("#heading")
-console.log(title)
-title.style.backgroundColor = "Pink"//inline.style
-title.style.color = "Magenta"   
+const title = document.querySelector("#heading");
+console.log(title);
+title.style.backgroundColor = "Pink"; //inline.style
+title.style.color = "Magenta";
 
-const listItems = document.querySelectorAll(".list-item")
-console.log(listItems)
-for(i=0;i<=4;i++){
-        listItems[i].style.color = "blue"
-        listItems[i].style.backgroundColor = "cyan"
-    }   
+const listItems = document.querySelectorAll(".list-item");
+console.log(listItems);
+for (i = 0; i <= 4; i++) {
+  listItems[i].style.color = "blue";
+  listItems[i].style.backgroundColor = "cyan";
+}
+
+const li = document.createElement("li")
+console.log(li)
+//add
+URL.append(li)
+
+li.setAttribute()
+//li.removeattribute()
+//for classes (add,remove,toggle,repalce,contains)
+li.classList.add("new-class");
