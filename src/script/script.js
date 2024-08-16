5e7f9546a2f7fe624eed58911e8ccf2d24cee685
Bhim <vanwu012@gmail.com>
2024-08-16 13:46:11 +0545

    new setup

---
+++ src/script/script.js
@@ -0,0 +1,11 @@
+// const title = document.getElementById("heading")
+// console.log(title)
+ 
+// const listItem = document.getElementsByClassName("list-item")
+// console.log(listItem)
+
+// const listTags = document.getElementsByTagName("li")
+// console.log(listTags)
+
+const container = document.querySelector("div")
+console.log(conatiner)
